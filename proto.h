4d8d7f3cae2e08177326476ead5555e47333fa12
Jonathan Neufeld <jneufeld@alumni.ubc.ca>
2011-12-28 09:54:27 -0800

    Remove in_checkmate

--- proto.h
+++ proto.h
@@ -55,7 +55,7 @@ int  valid_start_pos (int, int);
 int  valid_end_pos (int);
 int  opponent_player (int);
 int  move_piece (int, int);
-void unmove_piece (int, int);
+void unmove_piece (int, int, int);
 void gen_legal_moves (int, int, int *); 
 void gen_plegal_moves (int, int, int *);
 void remove_check_moves (int, int, int *);
