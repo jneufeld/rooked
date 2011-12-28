d948aa5432959ff89bf2969b1ea925b763411e36
Jonathan Neufeld <jneufeld@alumni.ubc.ca>
2011-12-28 11:18:59 -0800

    First commit, not much here

--- ai.c
+++ ai.c
@@ -1,1 +1,28 @@
 #include "proto.h"
+
+int board[BOARD_SIZE];
+
+/* Return the best move the AI can make. Use negascout!  */
+struct ai_move best_move ()
+{
+    struct ai_move move;
+    move.start_pos = 0;
+    move.end_pos   = 0;
+    int curr_util  = -999;
+
+    int legal_moves[BOARD_SIZE], i;
+    for (i = 0; i < BOARD_SIZE; i++) {
+        if (board[i] < chp_null) {
+            gen_legal_moves (BPLAYER, i, legal_moves);
+            // Now make the move (or subsequent moves) and evaluate
+        }
+    }
+
+    return move;
+}
+
+/* Return the utility of the board.  */
+int board_utility ()
+{
+    return 0;
+}
